2b718499f402ed8077e5b08b2589214443002a4e
muskanlohar234 <muskanlohar723@gmail.com>
2024-12-11 17:11:03 +0530

    Add files via upload

---
+++ Final exam/question-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+	//Write a program in C to find a cube of any number using user Define Function.
+	
+void main()
+{
+	cube(5);
+}
+
+void cube(int n)
+{
+	printf("The cube is %d",n*n);
+}
